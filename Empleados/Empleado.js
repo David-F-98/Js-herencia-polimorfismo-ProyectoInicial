98b152a21de15e463e309a81ef03d1ea86e87091
David Nieto <u2903149@unimilitar.edu.co>
2023-04-13 19:03:52 -0500

    POO-HERENCIA-POLIMORFISMO

--- Empleados/Empleado.js
+++ Empleados/Empleado.js
@@ -1,8 +1,28 @@
 export class Empleado {
     #nombre;
     #dni;
-    constructor(nombre, dni){
+    #salario;
+    #clave;
+    constructor(nombre, dni, salario){
         this.#nombre = nombre;
         this.#dni = dni;
+        this.#salario = salario;
+        this.#clave = '';
+    }
+
+    verBonificacion (bono){
+        return this.#salario;
+    }
+
+    _verBonificacion (bono){
+        return this.#salario+((this.#salario*bono)/100);
+    }
+    
+    asignarClave(clave){
+        this.#clave = clave;
+    }
+
+   autenticable(clave){
+        return clave == this.#clave;
     }
 }
--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './Cuentas/CuentaCorriente.js';
-import { CuentaAhorro } from './Cuentas/CuentaAhorro.js';
-import { cuentaNomina } from './Cuentas/CuentaNomina.js';
 import { Gerente } from './Empleados/Gerente.js';
 import { Empleado } from './Empleados/Empleado.js';
 import { Director } from './Empleados/Director.js';
+import { SistemaAutenticacion } from './Sistemaautenticacion.js';
+
+
+
+//const cliente2 = new Cliente('María','16979808','8989');
+
+const empleado = new Empleado('Juan Perez' , '12345613', 100000);
+empleado.asignarClave('12345');
+console.log(SistemaAutenticacion.login(empleado,'12345'));
+
+const gerente = new Gerente('Pedro Rivas', '23242', 12000);
+gerente.asignarClave('0000');
+console.log(SistemaAutenticacion.login(gerente,'0000'));
+
+//const director = new Director('Elena Moreno', '963258', 15000);
 
 const cliente = new Cliente('Leonardo','13804050','123224');
-const cliente2 = new Cliente('María','16979808','8989');
-
-const empleado = new Empleado('Juan Perez' , '12345613');
-const gerente = new Gerente('Pedro Rivas', '23242');
-const director = new Director('Elena Moreno', '963258');
-
-console.log(empleado);
-console.log(gerente);
-console.log(director);
+cliente.asignarClave('01111');
+console.log(SistemaAutenticacion.login(cliente,'1111'));
